93d594ba40d92179d2d290cea03e8f2378f602a1
DanieLuan <danielluanlourencol@gmail.com>
2023-11-08 14:22:12 -0300

    fix: little fixes in listView

--- app/src/main/java/br/ufrn/imd/productmanager/activities/ListaActivity.java
+++ app/src/main/java/br/ufrn/imd/productmanager/activities/ListaActivity.java
@@ -2,15 +2,72 @@ package br.ufrn.imd.productmanager.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 import br.ufrn.imd.productmanager.R;
+import br.ufrn.imd.productmanager.util.Product;
+import br.ufrn.imd.productmanager.database.ProductDbHelper;
+import br.ufrn.imd.productmanager.database.ProcuctContract.ProductData;
+
 
 public class ListaActivity extends AppCompatActivity {
+
+    List<Product> productList;
+    ListView productListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista);
+
+        productListView = (ListView) findViewById(R.id.List_ListView);
+        productList = new ArrayList<>();
+
+        List<String> nameProductList = new ArrayList<>();
+
+        getDatabaseProducts();
+
+        for(Product product:productList) {
+            nameProductList.add(product.getNameProduct());
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nameProductList);
+        productListView.setAdapter(adapter);
+
+    }
+
+    private void getDatabaseProducts(){
+        ProductDbHelper dbHelper = new ProductDbHelper(this);
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+
+        String stringQuery = "SELECT * from " + ProductData.TABLE_NAME;
+
+        Cursor query = db.rawQuery(stringQuery,null);
+
+        if(query.moveToFirst()){
+
+            while (query != null){
+                String tempProductCode = query.getString(1);
+                String tempProductName = query.getString(2);
+                String tempProductDesc = query.getString(3);
+                int tempProductQuantity = Integer.parseInt(query.getString(4));
+
+                Product tempProduct = new Product(tempProductCode, tempProductName, tempProductDesc, tempProductQuantity);
+                productList.add(tempProduct);
+
+                if(!query.moveToNext()){
+                    break;
+                }
+            }
+        }
     }
 }
--- app/src/main/java/br/ufrn/imd/productmanager/util/Product.java
+++ app/src/main/java/br/ufrn/imd/productmanager/util/Product.java
@@ -1,2 +1,48 @@
-package br.ufrn.imd.productmanager.util;public class Product {
+package br.ufrn.imd.productmanager.util;
+
+public class Product {
+
+    private String codeProduct;
+    private String nameProduct;
+    private String descProduct;
+    private int quantity;
+
+    public Product(String codeProduct, String nameProduct, String descProduct, int quantity) {
+        this.codeProduct = codeProduct;
+        this.nameProduct = nameProduct;
+        this.descProduct = descProduct;
+        this.quantity = quantity;
+    }
+
+    public String getCodeProduct() {
+        return codeProduct;
+    }
+
+    public void setCodeProduct(String codeProduct) {
+        this.codeProduct = codeProduct;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public String getDescProduct() {
+        return descProduct;
+    }
+
+    public void setDescProduct(String descProduct) {
+        this.descProduct = descProduct;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 }
